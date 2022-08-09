dfe3359cef29188516345fe411585494e510b8a0
invincible5146 <sachin.tiwari.civ19@itbhu.ac.in>
2022-08-09 12:43:02 +0530

    intro part api

--- src/main/java/com/splitwise/splitapp/Controller/ApiController.java
+++ src/main/java/com/splitwise/splitapp/Controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.splitwise.splitapp.Controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.splitwise.splitapp.Models.Dto.ExpenseDto;
@@ -27,6 +28,18 @@ public class ApiController {
     @GetMapping(value = "/authenticate")
     public Map<String,String> verifyUser(@RequestParam("username") String userName,@RequestParam("password") String password){
          return authenticationService.verify(userName,password);
+    }
+
+    @GetMapping(value = "")
+    public Map<String,String> start(){
+         Map<String,String> response= new HashMap<>();
+         response.put("Welcome to Splitwise app designed by Sachin", "Given Below Methods are allowed here");
+         response.put("/api/v1/authenticate", "To authenticate an user with phone number and password");
+         response.put("/api/v1/register","To register an user with name,phoneNumber and password");
+         response.put("/api/v1/expense-other-user", "To create an expense with other person and settle up ");
+         response.put("/api/v1/crete-group", "To create an group");
+         response.put("/api/v1/expense-group", "To create an expense in a group");
+         return response;
     }
 
     @PutMapping(value = "/register")
